5b83e2e99f642c88bd0a942d7b8a52d1d3998877
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-10-29 19:31:12 +0200

    Set up Express to serve static files

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const loggingMiddleware = (req, _, next) => {
-    const { method, url } = req;
-    const ipAddress = req.connection.remoteAddress;
-    console.log(`${method} ${url} - ${ipAddress}`);
-    next();
-};
-app.use(loggingMiddleware);
-
-app.get('/', (req, res) => res.send('Hello world!'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Express app listening on http://localhost:${port}`));
