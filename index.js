d5a6cbd8525ebccda34cbe4d7266ab36fb5908e8
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-11-24 19:21:42 +0200

    Format index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const publicDir = path.join(__dirname, 'public');
+const publicDir = path.join(__dirname, "public");
 app.use(express.static(publicDir));
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Express app listening on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Express app listening on http://localhost:${port}`)
+);
